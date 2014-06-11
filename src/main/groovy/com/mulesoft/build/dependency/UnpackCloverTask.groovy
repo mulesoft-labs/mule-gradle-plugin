04fed036cd7eeef54bed210f2094c009f86d584a
Juan Alberto López Cavallotti <juancavallotti@gmail.com>
2014-06-11 19:47:49 -0300

    Added facilities for unit testing with datamapper

---
+++ src/main/groovy/com/mulesoft/build/dependency/UnpackCloverTask.groovy
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2014 juancavallotti.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.mulesoft.build.dependency
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.plugins.JavaPluginConvention
+import org.gradle.api.tasks.Copy
+import org.gradle.api.tasks.TaskAction
+
+/**
+ * Expands the CloverETL Engine that allows unit tests against datamapper.
+ * Created by juancavallotti on 11/06/14.
+ */
+class UnpackCloverTask extends DefaultTask {
+
+    public static final String CLOVER_GROUP = 'com.cloveretl'
+    public static final String CLOVER_NAME = 'cloveretl-engine'
+
+    @TaskAction
+    void doUnpackClover() {
+
+        logger.debug('Trying to unpack Clover ETL for unit testing...')
+
+        //smoke test for this being an embedded app
+        if (!project.configurations.findByName('providedTestCompile')) {
+            logger.error('Unit testing datamapper is not supported on embedded projects.')
+            throw new IllegalStateException('Unit testing datamapper is not supported on embedded projects.')
+        }
+
+        //define an intermediate copy task
+        def copyTask = project.tasks.create('copyClover', Copy)
+
+
+        Set<File> providedTestRuntime = project.configurations.providedTestRuntime.files
+        Set<File> providedTestCompile = project.configurations.providedTestCompile.files
+
+        Set<File> finalFiles = providedTestRuntime - providedTestCompile
+
+        File clover = finalFiles.find({
+            it.name.startsWith('cloveretl-engine')
+        })
+
+        if (!clover) {
+            logger.error('Could not find Clover ETL engine, required to run the tests...')
+            throw new IllegalStateException('Could not find Clover ETL engine, required to run the tests...')
+        }
+
+        if (logger.debugEnabled) logger.debug("Found Clover ETL engine in $clover")
+
+
+        JavaPluginConvention convention = project.convention.findPlugin(JavaPluginConvention)
+
+        if (logger.debugEnabled) logger.debug("Clover will be unzipped into: ${convention.sourceSets.test.output.classesDir}")
+
+        copyTask.from project.zipTree(clover)
+        copyTask.into convention.sourceSets.test.output.classesDir
+        copyTask.execute()
+    }
+
+}
