36c3c65481c3f41a03b397e285313e59c4a57bb8
Juan Alberto López Cavallotti <juancavallotti@gmail.com>
2015-06-28 17:25:24 -0300

    Studio domain plugin now generates correct .classpath files issue #70

--- src/main/groovy/com/mulesoft/build/studio/StudioDomainPlugin.groovy
+++ src/main/groovy/com/mulesoft/build/studio/StudioDomainPlugin.groovy
@@ -15,13 +15,21 @@
  */
 package com.mulesoft.build.studio
 
+import com.mulesoft.build.MulePluginConvention
 import com.mulesoft.build.MulePluginExtension
 import com.mulesoft.build.domain.MuleDomainPlugin
+import com.mulesoft.build.domain.MuleDomainPluginConvention
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.Task
+import org.gradle.api.internal.tasks.DefaultSourceSet
+import org.gradle.api.internal.tasks.DefaultSourceSetContainer
+import org.gradle.api.tasks.SourceSet
+import org.gradle.plugins.ide.api.XmlFileContentMerger
 import org.gradle.plugins.ide.eclipse.EclipsePlugin
+import org.gradle.plugins.ide.eclipse.GenerateEclipseClasspath
 import org.gradle.plugins.ide.eclipse.model.BuildCommand
+import org.gradle.plugins.ide.eclipse.model.EclipseClasspath
 import org.gradle.plugins.ide.eclipse.model.EclipseModel
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -40,6 +48,11 @@ class StudioDomainPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
         logger.debug('Applying domain studio plugin...')
+
+        MuleDomainPluginConvention domainConvention = project.convention.create(MuleDomainPlugin.DOMAIN_CONVENTION_NAME, MuleDomainPluginConvention)
+
+        //domain subprojects will be mule studio.
+        domainConvention.subprojectsPlugin = StudioPlugin
 
         //apply the mule domain plugin.
         project.apply(plugin: MuleDomainPlugin)
@@ -74,7 +87,13 @@ class StudioDomainPlugin implements Plugin<Project> {
 
         }
 
-        Task currentTask = project.task('studio') << {
+        //configure the classpath task, special for this plugin.
+        project.plugins.withType(MuleDomainPlugin) {
+            configureEclipseClasspathTask(project, eclipseConfig)
+        }
+
+
+        Task studioTask = project.task('studio') << {
             logger.info('Updating mule studio project...')
 
             //get the mule project configuration
@@ -87,8 +106,40 @@ class StudioDomainPlugin implements Plugin<Project> {
             studioProject.createStudioProjectIfNecessary()
         }
 
-        currentTask.description = 'Update MuleStudio project metadata.'
-        currentTask.group = "IDE"
-        currentTask.dependsOn 'eclipse'
+        studioTask.description = 'Update MuleStudio project metadata.'
+        studioTask.group = "IDE"
+        studioTask.dependsOn 'eclipse'
     }
+
+    /**
+     * This is required because eclipse plugin only configures this task for JavaBase projects.
+     * @param project
+     */
+    private void configureEclipseClasspathTask(Project project, EclipseModel model) {
+
+        //we know it is there
+        EclipsePlugin ep = project.plugins.getPlugin(EclipsePlugin)
+        MulePluginConvention mulePluginConvention = project.convention.getByType(MulePluginConvention)
+
+        GenerateEclipseClasspath eclipseClasspath = project.tasks.create(EclipsePlugin.ECLIPSE_CP_TASK_NAME, GenerateEclipseClasspath)
+        ep.addWorker(eclipseClasspath)
+
+        //add the specific configuration
+        eclipseClasspath.description = "Generates the Eclipse classpath file."
+
+        eclipseClasspath.inputFile = project.file('.classpath')
+        eclipseClasspath.outputFile = project.file('.classpath')
+        eclipseClasspath.classpath = model.classpath
+        eclipseClasspath.classpath.containers 'org.eclipse.jdt.launching.JRE_CONTAINER'
+        eclipseClasspath.classpath.file = new XmlFileContentMerger(eclipseClasspath.xmlTransformer)
+
+        //define the two source sets that are interesting to us.
+        DefaultSourceSet dss = new DefaultSourceSet('main', project.fileResolver)
+
+        dss.resources.srcDirs(mulePluginConvention.domainSourceDir, "src/${dss.name}/resources")
+
+        eclipseClasspath.classpath.sourceSets = [dss]
+
+    }
+
 }
